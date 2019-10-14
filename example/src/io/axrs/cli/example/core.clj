eddcf6f9c175d42356d0a097a11ca054bdfc5c35
Alexander Scott <xander@axrs.io>
2019-10-14 21:37:10 +1000

    example cli project

---
+++ example/src/io/axrs/cli/example/core.clj
@@ -0,0 +1,10 @@
+(ns io.axrs.cli.example.core
+  (:require
+    [io.jesi.backpack.random :as rnd]))
+
+(defn -main [& args]
+  (println "Hello! from the example lib")
+  (println "Clojure version: " *clojure-version*)
+  (println "Command line args: " *command-line-args*)
+  (println "Args: " args)
+  (println "Random string:" (rnd/alpha-numeric)))
