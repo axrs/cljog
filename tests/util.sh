17a36d33abb7089db60f4ee83e31abc9f2742414
Alexander Scott <xander@axrs.io>
2019-10-28 20:28:39 +1000

    Indentation fix

--- tests/util.sh
+++ tests/util.sh
@@ -2,10 +2,10 @@ array_contains () {
 	local seeking=$1; shift
 	local in=1
 	for element; do
-			if [[ ${element} == *"$seeking"* ]]; then
-					in=0
-					break
-			fi
+		if [[ ${element} == *"$seeking"* ]]; then
+			in=0
+			break
+		fi
 	done
-	return $in
+	return ${in}
 }
